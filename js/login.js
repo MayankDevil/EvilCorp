6fa13a7dd2cd1f30a4b1be39febbafba66407e80
मयंक <mastermayank91@gmail.com>
2024-02-29 16:30:45 +0000

    Update login

--- js/login.js
+++ js/login.js
@@ -13,36 +13,71 @@ try
 
 	let login_btn = document.querySelector(" #login .btn ")
 
+	let validation = document.getElementById('validate')
+
+	validation.innerText = ''
+
+	validation.classList.remove('isValid')
+
+	validation.classList.remove('isUnvalid')
+
 	login_btn.onclick = function()
 	{
-		let value1 = '9211420420'
+		const admin = {
+			name : '7027_21mayank@gc11.ac.in',
+			key : '9211420'
+		}
 
-		let value2 = 'openlock'
-
-		if (login_fld[0].value === value1)
+		if (login_fld[0].value == "" || login_fld.value == "")
 		{
-			if (login_fld[1].value === value2)
+			console.log('\n_[error] : empty field, please!')
+
+			validation.classList.add('isUnvalid')
+			validation.innerText = `_[error] : empty field`
+			
+			return;
+		}
+		else if (login_fld[0].value === admin.name || login_fld[0].value === 'rootUser')
+		{
+			if (login_fld[1].value === admin.key)
 			{
-				document.getElementById('login').innerHTML = personal_Data()
+				console.error(`\n _[unvalid] : password`)
+				validation.classList.add('isValid')
+				validation.innerText = `_[unvalid] : password`
+
+				setTimeout(databaseLayout(), 2000)
 			}
 			else
 			{
-				alert(`Contact id is unmatch in database! \n Please Check`)
-				console.error(`Contact id is unmatch in database! \n Please Check`)
+				console.error(`\n _[unvalid] : password`)
+				validation.classList.add('isUnvalid')
+				validation.innerText = `_[unvalid] : password`
 			}
 		}
 		else
 		{
-			alert(`Contact id is unmatch in database! \n Please Check`)
-			console.error(`Contact id is unmatch in database! \n Please Check`)
+			console.error(`\n _[unvalid] : admin name`)
+
+			validation.classList.add('isUnvalid')
+			validation.innerText = `_[unvalid] : admin name`
 		}
 	}
-
-	function personal_Data()
+    
+    /*
+        -------------------------------------------
+        | display database layout with fetch data | 
+        -------------------------------------------
+    */
+    
+	function databaseLayout()
 	{
-		return (
-			`<h2> Login Success but </h2>`
-		)
+		document.body.innerHTML = (`
+			<section class="container">
+			
+				<h2> DataBase </h2>
+			
+			</section>
+		`)
 	}
 }
 catch(error)
